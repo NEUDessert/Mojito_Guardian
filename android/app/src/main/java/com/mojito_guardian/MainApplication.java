b5b91c800b2846297edb8427245c3c6faf315fc8
Lawrence Lee <596701600@qq.com>
2016-10-22 17:20:55 +0800

    jpush added

--- android/app/src/main/java/com/mojito_guardian/MainApplication.java
+++ android/app/src/main/java/com/mojito_guardian/MainApplication.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import in.sriraman.sharedpreferences.RNSharedPreferencesReactPackage;
+import cn.jpush.reactnativejpush.*;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNSharedPreferencesReactPackage()
+          new RNSharedPreferencesReactPackage(),
+          new JPushPackage(true, true)
       );
     }
   };
